2b382f8f959130e4160f1d0d6763dae09c5afe69
david <davidmughogho100@gmail.com>
2026-08-29 18:49:09 +0200

    have made a delta time lesson

---
+++ lessons/physics/sdl_delta_time.c
@@ -0,0 +1,72 @@
+/*
+ This tutorial shows how to capture keyboard input in
+ sdl2.
+
+ Compilation commands:
+ ====================
+	 Linux ==>  gcc sdl_delta_time.c -o delta -lSDL2  && ./delta
+	 Mac OS ==> 
+	 Windows ==>
+*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+
+
+int main()
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *win = SDL_CreateWindow("sdl delta time calculation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 900,600,0);
+	SDL_Renderer *render = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+
+	// sdl delta time calculation
+	Uint64 old_time =  SDL_GetTicks();
+	float delta_time = 0.0;
+
+	bool running = true;
+	SDL_Event ev;
+	while (running)
+	{
+		// lets get the current time
+		Uint64 current_time = SDL_GetTicks();
+
+		while (SDL_PollEvent(&ev))
+		{
+			if (ev.type == SDL_QUIT) 
+			{
+				running = false;
+			}
+
+			// keyboard input in sdl2
+			if (ev.type == SDL_KEYDOWN)
+			{
+
+				if (ev.key.keysym.sym == SDLK_ESCAPE)
+				{
+					running = false;
+				}
+			}
+		}
+
+		// time difference betwwen the two frames
+		// we devide by 1000.0f to get the delta time in seconds
+		delta_time = (current_time - old_time) / 1000.0f;
+		printf("delta time in seconds: %f\n", delta_time);
+		// update old_time to and set it to this frames time
+		old_time = current_time;
+
+		SDL_SetRenderDrawColor(render, 0,0,0,255);
+		SDL_RenderClear(render);
+
+		SDL_RenderPresent(render);
+	}
+
+	// be sure to clean up
+	// hope you remember we are using c
+	SDL_DestroyRenderer(render);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
+}
+
